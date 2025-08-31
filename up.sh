1bd3b0c72bda751ff6e20693735e79943db94d7e
Hugo Meireles <estudantecyber@gmail.com>
2025-08-31 06:06:44 -0300

    burp e splunk containers

--- up.sh
+++ up.sh
@@ -5,4 +5,6 @@ docker compose -f waf2py/docker-compose.yml \
                -f gophish/docker-compose.yml \
                -f inbucket/docker-compose.yml \
                -f openvas/docker-compose.yml \
+               -f burp-suite/docker-compose.yml \
+               -f splunk/docker-compose.yml \
                up -d
