db95123993661ad690047f6c3070ba710b59e171
Hugo Meireles <estudantecyber@gmail.com>
2025-08-31 04:52:05 -0300

    Update down.sh

--- down.sh
+++ down.sh
@@ -2,7 +2,7 @@
 docker compose -f waf2py/docker-compose.yml \
                -f hashicorp_vault/docker-compose.yml \
                -f nessus-essentials/docker-compose.yml \
-	       -f gophish/docker-compose.yml \
-	       -f inbucket/docker-compose.yml \
+               -f gophish/docker-compose.yml \
+               -f inbucket/docker-compose.yml \
                down
 
