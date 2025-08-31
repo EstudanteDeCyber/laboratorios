527442b6ce3e29d2adbc9c46c006013842c1722f
Hugo Meireles <estudantecyber@gmail.com>
2025-08-31 12:50:41 -0300

    Diversos containers

--- scripts/install_burpsuite_container.sh
+++ scripts/install_burpsuite_container.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-cd ..
+cd /home/vagrant
 git clone https://github.com/hexcowboy/docker-burp-suite-community.git && cd docker-burp-suite-community
 sed -i 's|FROM openjdk:11-jre-slim|FROM openjdk:21-jdk-slim-bullseye|' Dockerfile
 sed -i 's|ttf-dejavu|ttf-bitstream-vera|' Dockerfile
