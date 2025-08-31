4d6c2bf26cca119cf665234c012190ded26477ac
Hugo Meireles <estudantecyber@gmail.com>
2025-08-31 08:53:29 -0300

    Diversos containers

--- scripts/docker_provision.sh
+++ scripts/docker_provision.sh
@@ -3,8 +3,8 @@
 timedatectl set-timezone America/Sao_Paulo
 
 ## Update da maquina virtual
-apt update && apt-mark hold openssh-server
-NEEDRESTART_MODE=a apt -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" upgrade 
+apt update
+NEEDRESTART_MODE=a apt -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confnew" upgrade
 
 ## Verificando e removendo instalação antiga do Docker, caso haja
 echo
@@ -17,9 +17,28 @@ done
 
 ## Instalar PRÉ-REQUISITOS
 echo
-echo "Instalação de pacotes pre-requisitos..."
+echo "Instalacao de pacotes pre-requisitos..."
 echo
 apt install -y htop unzip curl git ca-certificates lsb-release gnupg docker-compose
+
+## Adicionando a instalação e configuração do Chrony
+echo
+echo "Instalando e configurando Chrony para sincronizar com ntp.br..."
+echo
+# Parar o serviço padrão de sincronização de tempo para evitar conflito
+sudo timedatectl set-ntp false
+# Remover o arquivo de configuração de exemplo
+rm -f /etc/chrony/chrony.conf
+# Instalar Chrony
+apt install -y chrony
+# Adicionar a configuração para sincronizar com servidores ntp.br
+echo "server ntp.br iburst" | sudo tee /etc/chrony/chrony.conf
+# Reiniciar o serviço Chrony
+sudo systemctl restart chrony
+# Habilitar o serviço Chrony
+sudo systemctl enable chrony
+# Verificar o status da sincronização
+chronyc sources
 
 ## Instalar Docker via script oficial
 echo
