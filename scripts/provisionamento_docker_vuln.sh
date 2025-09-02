a7fed88ebfd01046ec6a8812b486ef0692da48f1
Hugo Meireles <estudantecyber@gmail.com>
2025-09-02 19:37:24 -0300

    Diversos containers

--- scripts/provisionamento_docker_vuln.sh
+++ scripts/provisionamento_docker_vuln.sh
@@ -90,4 +90,4 @@ bash listar-container-portas.sh
 echo "Exibindo mensagem final..."
 bash msg_final.sh 10.10.10.102
 
-echo "Provisionamento do nó concluído!"
+echo "Provisionamento do no concluido!"
