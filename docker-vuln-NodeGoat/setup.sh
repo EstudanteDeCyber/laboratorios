f9896f2b55c9ed7273461ff87c2e9f4ec4714654
Hugo Meireles <estudantecyber@gmail.com>
2025-09-02 11:57:41 -0300

    Diversos containers

--- docker-vuln-NodeGoat/setup.sh
+++ docker-vuln-NodeGoat/setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define diretório de trabalho
-WORKDIR="/home/vagrant/docker-vuln-NodeGoat"
+WORKDIR="/home/vagrant/lab-sec/docker-vuln-NodeGoat"
 
 # Clona o repositório se ainda não existir
 cd "$WORKDIR"
