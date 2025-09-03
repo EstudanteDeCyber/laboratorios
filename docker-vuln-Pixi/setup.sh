7e0a851b29bd788c25a735a47385a5265a6e2201
Hugo Meireles <estudantecyber@gmail.com>
2025-09-03 12:18:11 -0300

    Diversos containers

--- docker-vuln-Pixi/setup.sh
+++ docker-vuln-Pixi/setup.sh
@@ -10,11 +10,11 @@ cd Pixi
 sed -i '1d' docker-compose.yaml
 sed -i '1d' docker-compose.yaml
 
-# 2) Adicionar restart e e renomear container_name para "db-Pixi" 
+# 2) Adicionar restart e e renomear container_name para "Pixi-db" 
 sed -i '/image:/a \    restart: unless-stopped' docker-compose.yaml
-sed -i 's/pixidb/db-Pixi/g' docker-compose.yaml
+sed -i 's/pixidb/Pixi-db/g' docker-compose.yaml
 
-# 3) Adicionar restart e container_name em "app-Pixi"
-sed -i '/build/a \    restart: unless-stopped\n    container_name: app-Pixi' docker-compose.yaml
+# 3) Adicionar restart e container_name em "Pixi-app"
+sed -i '/build/a \    restart: unless-stopped\n    container_name: Pixi-app' docker-compose.yaml
 
 docker compose up -d
