88010070150e01a572bf5bdc0a38071b990e068d
Hugo Meireles <estudantecyber@gmail.com>
2025-09-05 12:13:57 -0300

    Diversos containers

--- scripts/openvpn.sh
+++ scripts/openvpn.sh
@@ -2,34 +2,32 @@
 
 set -e
 
-# === CONFIGURAÇÕES ===
+# === CONFIGURACOES ===
 VPN_SUBNET="172.27.224.0/20"         # Subnet padrão do OpenVPN-AS
 LAN_SUBNET="10.10.10.0/24"
 LAN_INTERFACE="eth1"
-VPN_DATA_DIR="/home/vagrant/openvpn/openvpn-data"
-DOCKER_COMPOSE_FILE="/home/vagrant/openvpn/docker-compose.yml"
 #OPENVPN_ADMIN_PASS="changeme123"     # Senha padrão do admin
 SERVER_HOST="192.168.1.10"           # IP de acesso ao servidor web
 
-# === CRIA DIRETÓRIO DE DADOS ===
-mkdir -p "$VPN_DATA_DIR"
+# === CRIA DIRETORIO DE DADOS ===
+mkdir -p /home/vagrant/openvpn/openvpn-data
 
 # === SOBE O CONTAINER ===
 echo "[+] Subindo OpenVPN Access Server..."
-docker compose -f "$DOCKER_COMPOSE_FILE" up -d
+docker compose -f /home/vagrant/openvpn/docker-compose.yml up -d
 
 # === AGUARDA SUBIR ===
 echo "[+] Aguardando OpenVPN iniciar..."
 sleep 15
 
 # === DEFINE SENHA DO ADMIN ===
-#echo "[+] Configurando usuário admin..."
+#echo "[+] Configurando usuario admin..."
 #docker exec openvpn-as /usr/local/openvpn_as/scripts/sacli --user openvpn --key "prop_superuser" --value "true" UserPropPut
 #docker exec openvpn-as /usr/local/openvpn_as/scripts/sacli --user openvpn --key "type" --value "user_pass" UserPropPut
 #docker exec openvpn-as /usr/local/openvpn_as/scripts/sacli --user openvpn --new_pass "$OPENVPN_ADMIN_PASS" SetLocalPassword
 
 # === CONFIGURA ROTEAMENTO PARA A REDE INTERNA ===
-echo "[+] Configurando acesso à rede interna via CLI..."
+echo "[+] Configurando acesso a rede interna via CLI..."
 docker exec openvpn-as /usr/local/openvpn_as/scripts/sacli --key "vpn.server.routing.gateway_redirect" --value "false" ConfigPut
 docker exec openvpn-as /usr/local/openvpn_as/scripts/sacli --key "vpn.server.routing.reroute_dns" --value "false" ConfigPut
 docker exec openvpn-as /usr/local/openvpn_as/scripts/sacli --key "vpn.server.routing.private_network.0" --value "$LAN_SUBNET" ConfigPut
@@ -41,7 +39,7 @@ sysctl -w net.ipv4.ip_forward=1
 grep -q "net.ipv4.ip_forward = 1" /etc/sysctl.conf || echo "net.ipv4.ip_forward = 1" >> /etc/sysctl.conf
 
 # === ADICIONA REGRA NAT ===
-echo "[+] Adicionando NAT para tráfego da VPN para rede interna..."
+echo "[+] Adicionando NAT para trafego da VPN para rede interna..."
 iptables -t nat -C POSTROUTING -s "$VPN_SUBNET" -d "$LAN_SUBNET" -j MASQUERADE 2>/dev/null \
   || iptables -t nat -A POSTROUTING -s "$VPN_SUBNET" -d "$LAN_SUBNET" -j MASQUERADE
 netfilter-persistent save
