ab81ba8f691ee4be60d0cc994ea634b86793a1d2
Hugo Meireles <estudantecyber@gmail.com>
2025-09-05 22:51:04 -0300

    Diversos containers

--- docker-tools-django-DefectDojo/setup.sh
+++ docker-tools-django-DefectDojo/setup.sh
@@ -14,4 +14,5 @@ docker compose up -d
 
 # Obtain admin credentials. The initializer can take up to 3 minutes to run.
 # Use docker compose logs -f initializer to track its progress.
-docker compose logs initializer | grep "Admin password:"
+# docker compose logs initializer | grep "Admin password:"
+sed -i '/image:/a \    restart: unless-stopped' docker-compose.yml
