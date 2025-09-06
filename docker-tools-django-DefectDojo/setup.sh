64e3f547a2d70a30140d451a76b47d04919f29c7
Hugo Meireles <estudantecyber@gmail.com>
2025-09-06 00:17:10 -0300

    Diversos containers

--- docker-tools-django-DefectDojo/setup.sh
+++ docker-tools-django-DefectDojo/setup.sh
@@ -2,12 +2,6 @@
 git clone https://github.com/DefectDojo/django-DefectDojo
 cd django-DefectDojo
 
-# Building Docker images
-docker compose build
-
-# Adicionar restart automatico
-sed -i '/image:/a \    restart: unless-stopped' docker-compose.yml
-
 # Run the application (for other profiles besides postgres-redis see  
 # https://github.com/DefectDojo/django-DefectDojo/blob/dev/readme-docs/DOCKER.md)
 docker compose up -d
@@ -15,4 +9,6 @@ docker compose up -d
 # Obtain admin credentials. The initializer can take up to 3 minutes to run.
 # Use docker compose logs -f initializer to track its progress.
 # docker compose logs initializer | grep "Admin password:"
+
+# Adicionar restart automatico
 sed -i '/image:/a \    restart: unless-stopped' docker-compose.yml
